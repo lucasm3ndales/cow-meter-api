69d9e5f27a4ec4196763331cab174ce69bfbaf3f
lucas_dev <82479610+lucasm3ndales@users.noreply.github.com>
2023-09-12 14:21:08 -0300

    T-0 up do projeto no repositório remoto

---
+++ src/test/java/br/csi/cow_meter_api/CowMeterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.csi.cow_meter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowMeterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
