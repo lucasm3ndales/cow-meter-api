e28fef9b7ffd07f89708d09092666c677e2882f6
lucas_dev <82479610+lucasm3ndales@users.noreply.github.com>
2023-09-12 14:42:30 -0300

    T-0 config das pastas e estrutura de projeto

--- src/main/java/br/csi/cow_meter_api/CowMeterApiApplication.java
+++ src/main/java/br/csi/cow_meter_api/CowMeterApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CowMeterApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CowMeterApiApplication.class, args);
     }
-
 }
