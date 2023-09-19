7335780b641e881e30b60f31daccf67ba0da1584
coradini1 <gabe.cora1@hotmail.com>
2023-09-19 14:18:08 -0300

    feat: models bovino and raca

--- src/main/java/br/csi/cowMeterApi/models/Bovino.java
+++ src/main/java/br/csi/cowMeterApi/models/Bovino.java
@@ -1,5 +1,58 @@
 package br.csi.cowMeterApi.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "bovino")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bovino {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Raca raca;
+
+    private String nome;
+    private BigDecimal peso;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataNasc;
+
+    @Enumerated(EnumType.STRING)
+    private Role sexo;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_entrada_cio")
+    private Date dataEntradaCio;
+
+    private String observacoes;
+    private boolean castrado;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_bovino")
+    private TipoBovino tipoBovino;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
 
 }
--- src/main/java/br/csi/cowMeterApi/models/Raca.java
+++ src/main/java/br/csi/cowMeterApi/models/Raca.java
@@ -1,4 +1,44 @@
 package br.csi.cowMeterApi.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "raca")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Raca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "descricao", columnDefinition = "TEXT")
+    private String descricao;
+
+    @OneToMany(mappedBy = "raca") // Mapeamento inverso no campo 'raca' em Bovino
+    private Set<Bovino> bovinos;
+
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
