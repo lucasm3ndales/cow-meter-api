d9eb0c5c30cf4b91a810c50e18ba63c6399ff266
coradini1 <gabe.cora1@hotmail.com>
2023-12-12 09:24:40 -0300

    id fix

--- src/main/resources/db/migration/V2__insertdata.sql
+++ src/main/resources/db/migration/V2__insertdata.sql
@@ -4,6 +4,7 @@ VALUES (1, 'Mark', '13688811062', '$2a$12$5EnUPOUtkOSrDiOvLM.GROe6xIpBrs9l5NuscO
        (2, 'Gordon', '34856757091', '$2a$12$5EnUPOUtkOSrDiOvLM.GROe6xIpBrs9l5NuscONyWGcY36WS0421a', 'CRIADOR', true),
        (3, 'Zac', '23512586066', '$2a$12$5EnUPOUtkOSrDiOvLM.GROe6xIpBrs9l5NuscONyWGcY36WS0421a', 'CRIADOR', true),
        (4, 'Jane', '07020974031', '$2a$12$5EnUPOUtkOSrDiOvLM.GROe6xIpBrs9l5NuscONyWGcY36WS0421a', 'CRIADOR', true);
+SELECT setval('usuario_id_seq', COALESCE((SELECT MAX(id)+1 FROM usuario), 1), false);
 
 -- Inserts for 'raca' table
 INSERT INTO raca (id, nome, descricao, criado_em, atualizado_em)
@@ -11,6 +12,7 @@ VALUES (1, 'Holstein', 'Black and white dairy cattle', '2023-01-01', '2023-01-01
        (2, 'Angus', 'Black beef cattle', '2023-01-02', '2023-01-02'),
        (3, 'Hereford', 'Red and white beef cattle', '2023-01-03', '2023-01-03'),
        (4, 'Jersey', 'Brown dairy cattle', '2023-01-04', '2023-01-04');
+SELECT setval('raca_id_seq', COALESCE((SELECT MAX(id)+1 FROM raca), 1), false);
 
 -- Inserts for 'bovino' table
 INSERT INTO bovino (id, raca_id, nome, data_nasc, sexo, observacoes, castrado, tipo_bovino, criado_em, atualizado_em,
@@ -19,10 +21,11 @@ VALUES (1, 1, 'Bessie', '2020-05-15', 'FEMEA', 'Black and white spotted', false,
        (2, 2, 'Bob', '2021-02-20', 'MACHO', 'Solid black color', true, 'TOURO', '2023-01-06', '2023-01-06', 2),
        (3, 3, 'Daisy', '2022-09-10', 'FEMEA', 'Red and white markings', false, 'NOVILHA', '2023-01-07', '2023-01-07', 3),
        (4, 4, 'Charlie', '2023-04-01', 'MACHO', 'Brown color', true, 'BEZERRO', '2023-01-08', '2023-01-08', 4);
-
+SELECT setval('bovino_id_seq', COALESCE((SELECT MAX(id)+1 FROM bovino), 1), false);
 -- Inserts for 'saude' table
 INSERT INTO saude (id, bovino_id, peso, data_entradaCio, tipo_tratamento, data_tratamento, medicamentos, observacoes, criado_em, atualizado_em)
 VALUES (1, 1, 500.0, '2023-01-09', 'Vaccination', '2023-01-10', 'Vaccine A, Vaccine B', 'Regular checkup', '2023-03-05', '2023-03-05'),
        (2, 2, 700.0, '2023-01-11', 'Deworming', '2023-01-12', 'Dewormer X', 'Monitoring weight gain','2023-01-05', '2023-01-05'),
        (3, 3, 450.0, NULL, 'Antibiotic', '2023-01-13', 'Antibiotic Y', 'Recovering from illness','2024-01-05','2024-01-05'),
        (4, 4, 300.0, NULL, 'Dipirona', '2023-03-14', 'Dipirona Xx', 'obses', '2023-01-28', '2023-05-23');
+SELECT setval('saude_id_seq', COALESCE((SELECT MAX(id)+1 FROM saude), 1), false);
